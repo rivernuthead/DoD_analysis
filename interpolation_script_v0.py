fdb3539fa33c1f34908b47c5eb8cf41748b8d8c9
rivernuthead <enrico.pandrin@unitn.it>
2022-04-06 14:40:49 +0200

    latest

--- interpolation_script_v0.py
+++ interpolation_script_v0.py
@@ -12,7 +12,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy import optimize as opt
 
-start = time.time() # Set initial time
 ######################################################################################
 # FUNCTIONS
 ######################################################################################
@@ -52,6 +51,11 @@ def func_ln(x,A,B):
     y=A*np.ln(x/B)
     return y
 
+###############################################################################
+# SCRIPT PARAMETERS
+###############################################################################
+start = time.time()
+N = 4 # Number of series to extract
 
 ###############################################################################
 # SETUP FOLDERS
@@ -64,24 +68,84 @@ run_dir = os.path.join(w_dir, 'surveys') # Set path were surveys for each runs a
 
 
 
-# EXTRACT RUNS
+# EXTRACT ALL RUNS
 RUNS = [] # Initialize RUS array
 for RUN in sorted(os.listdir(run_dir)):
-    if RUN.startswith('q') and RUN.endswith('_2'):
+    if RUN.startswith('q'):
         RUNS = np.append(RUNS, RUN)
+    
+# MANUAL RUNS SELECTION
+RUNS = ['q07_1', 'q10_1', 'q10_2', 'q15_1', 'q15_2', 'q20_1', 'q20_2']
 
 # Per ogni run leggere i dati dal file di testo dei valori di scavi, depositi e active width.
 # I dati vengono posti all'interno di in dizionario'
-d = {}
+d = {} # Dictionary for runs data. Each entry is a report (output for DoD_analysis script)
+int_d = {} # Dictionary for data series to be interpolated. An estraction of data from dictionary d
+param_d = {} # Dictionary of all the interpolation parameters
+
+####
+# For loop over all runs
+####
 for run in RUNS:
-    sco_report = np.loadtxt(os.path.join(data_folder, run + '_sco_report.txt'), delimiter = ',')
-    dep_report = np.loadtxt(os.path.join(data_folder, run + '_dep_report.txt'), delimiter = ',')
-    morphW_report = np.loadtxt(os.path.join(data_folder, run + '_morphWact_report.txt'), delimiter = ',')
-    d["sco_data_{0}".format(run)] = sco_report[:-3,:4]
+    
+    # Extract the number of survey files in the survey folder
+    # Needed to know the dimension of the matrix
+    files = []
+    for f in sorted(os.listdir(os.path.join(run_dir,run))):
+        path = os.path.join(run_dir,run, f)
+        if os.path.isfile(path) and f.endswith('.txt') and f.startswith('matrix_bed_norm_'+run+'s'):
+            files = np.append(files, f)
+    file_N = files.shape[0]-1 # Number of DoD
+    
+    # Create the d dictionary entry loading report files from DoD_analysis.py output folder
+    d["sco_data_{0}".format(run)] = np.loadtxt(os.path.join(data_folder, run + '_sco_report.txt'), delimiter = ',')
     d["dep_data_{0}".format(run)] = np.loadtxt(os.path.join(data_folder, run + '_dep_report.txt'), delimiter = ',')
     d["morphWact_data_{0}".format(run)] = np.loadtxt(os.path.join(data_folder, run + '_morphWact_report.txt'), delimiter = ',')
     
-
+    # Fill int_d dictionary with an extraction of scour, deposition and morphological active width reports
+    # The script create a number of N indipendent sereis of data and append the last file_N - N as the averaged values.
+    # For N = 4 and file_N = 9
+    # DoD step0 | 1-0  | 2-1 |  3-2 |  4-3 |  5-4   6-5   7-6   8-7   9-8   average0   STDEV
+    # DoD step1 | 2-0  | 3-1 |  4-2 |  5-3 |  6-4   7-5   8-6   9-7         average1   STDEV
+    # DoD step2 | 3-0  | 4-1 |  5-2 |  6-3 |  7-4   8-5   9-6               average2   STDEV
+    # DoD step3 | 4-0  | 5-1 |  6-2 |  7-3 |  8-4   9-5                     average3   STDEV
+    # DoD step4 | 5-0  | 6-1 |  7-2 |  8-3 |  9-4                           average4   STDEV
+    # DoD step5   6-0   7-1   8-2   9-3                                   | average5 | STDEV
+    # DoD step6   7-0   8-1   9-2                                         | average6 | STDEV
+    # DoD step7   8-0   9-1                                               | average7 | STDEV
+    # DoD step8   9-0                                                     | average8 | STDEV
+    # as:
+    # |   1-0    |    2-1   |    3-2   |    4-3   |
+    # |   2-0    |    3-1   |    4-2   |    5-3   |
+    # |   3-0    |    4-1   |    5-2   |    6-3   |
+    # |   4-0    |    5-1   |    6-2   |    7-3   |
+    # |   5-0    |    6-1   |    7-2   |    8-3   |
+    # | average5 | average5 | average5 | average5 |
+    # | average6 | average6 | average6 | average6 |
+    # | average7 | average7 | average7 | average7 |
+    # | average8 | average8 | average8 | average8 |
+    
+    # Create dictionary entries with zeros
+    int_d["int_sco_data_{0}".format(run)] = np.zeros((file_N, N))
+    int_d["int_dep_data_{0}".format(run)] = np.zeros((file_N, N))
+    int_d["int_morphWact_data_{0}".format(run)] = np.zeros((file_N, N))
+    
+    # Fill int_d dictionary with data as above
+    for i in range(0,N):
+        int_d["int_dep_data_" + run][:file_N-N+1,:N] = d["dep_data_" + run][:file_N-N+1,:N]
+        int_d["int_dep_data_" + run][file_N-N+i,:] = d["dep_data_" + run][file_N-N+i,-2]
+        int_d["int_sco_data_" + run][:file_N-N+1,:N] = d["sco_data_" + run][:file_N-N+1,:N]
+        int_d["int_sco_data_" + run][file_N-N+i,:] = d["sco_data_" + run][file_N-N+i,-2]
+        int_d["int_morphWact_data_" + run][:file_N-N+1,:N] = d["morphWact_data_" + run][:file_N-N+1,:N]
+        int_d["int_morphWact_data_" + run][file_N-N+i,:] = d["morphWact_data_" + run][file_N-N+i,-2]
+        
+    # INTERPOLATION
+    param_d["param_int_{0}".format(run)] = np.zeros((2,2)) # Initialize dictionary
+    xData = int_d["int_dep_data_" + run][:,0]
+    dep_par, dep_intCurve, dep_covar =  interpolate(func_exp, xData, np.linspace(0,int(file_N), 1), ic=None, bounds=(-np.inf, np.inf))
+    
+    
+    
 # Per ogni run plottare i grafici degli andamenti interpolare ogni curva con la propria funzione
 
 # Stampare in un file txt i valori dei parametri della funzione interpolatrice e la loro deviazione standard
